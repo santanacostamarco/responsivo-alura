102c7bbae8be3f18e0e8ed9bd06ee6bc4aac72fa
Marco Antonio <santannacostamarco@gmail.com>
2018-02-21 10:13:20 -0300

    fim do curso de design responsivo

--- js/menu.js
+++ js/menu.js
@@ -1,9 +1,17 @@
 var botaoExibir = document.querySelector(".exibe-menu");
 var botaoEsconder = document.querySelector(".esconde-menu");
-var menu = document.querySelector(".barra-nav");
-botaoExibir.addEventListener("click", function(){
-    menu.classList.add("menu-mobile");
-});
-botaoEsconder.addEventListener("click", function(){
-    menu.classList.remove("menu-mobile");
-});
+var menu = document.querySelector(".nav-overlay");
+// botaoExibir.addEventListener("click", function(){
+//     menu.classList.add("menu-mobile");
+// });
+// botaoEsconder.addEventListener("click", function(){
+//     menu.classList.remove("menu-mobile");
+// });
+document.querySelector('.exibe-menu').onclick = function() {
+    document.documentElement.classList.add('menu-mobile');
+};
+
+document.querySelector('.esconde-menu').onclick = function() {
+    document.documentElement.classList.remove('menu-mobile');
+};
+
